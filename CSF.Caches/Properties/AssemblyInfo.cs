76b615e8b3f8ad549cf631c7b8b3970877c4d9f9
Craig Fowler <craig@csf-dev.com>
2016-12-08 20:55:46 +0000

    WIP #1 - Tidy up and remove unneeded files

--- CSF.Caches/Properties/AssemblyInfo.cs
+++ CSF.Caches/Properties/AssemblyInfo.cs
@@ -2,9 +2,9 @@
 // AssemblyInfo.cs
 //
 // Author:
-//       Craig Fowler <craig@craigfowler.me.uk>
+//       Craig Fowler <craig@csf-dev.com>
 //
-// Copyright (c) 2016 Craig Fowler
+// Copyright (c) 2015 CSF Software Limited
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -23,30 +23,21 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
+
 using System.Reflection;
 using System.Runtime.CompilerServices;
+using System;
 
-// Information about this assembly is defined by the following attributes.
-// Change them to the values specific to your project.
-
+[assembly: CLSCompliant(true)]
 [assembly: AssemblyTitle("CSF.Caches")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyDescription("Base types and an interface for implementing caches; currently only an in-memory thread-safe cache.")]
 [assembly: AssemblyCompany("CSF Software Limited")]
-[assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("Craig Fowler")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyCopyright("CSF Software Limited")]
 
-// The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
-// The form "{Major}.{Minor}.*" will automatically update the build and revision,
-// and "{Major}.{Minor}.{Build}.*" will update just the revision.
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
 
-[assembly: AssemblyVersion("1.0.*")]
-
-// The following attributes are used to specify the signing key for the assembly,
-// if desired. See the Mono documentation for more information about signing.
-
-//[assembly: AssemblyDelaySign(false)]
-//[assembly: AssemblyKeyFile("")]
-
+[assembly: AssemblyVersion("1.0.0")]
