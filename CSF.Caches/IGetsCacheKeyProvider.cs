c7fa23d9ef48024883fa829217f3d646c002ee7c
Craig Fowler <craig@csf-dev.com>
2020-04-19 13:31:22 +0100

    Resolve #6 - Add general-use cache key provider & factory

---
+++ CSF.Caches/IGetsCacheKeyProvider.cs
@@ -0,0 +1,42 @@
+﻿//
+// IGetsCacheKeyFromObjects.cs
+//
+// Author:
+//       Craig Fowler <craig@csf-dev.com>
+//
+// Copyright (c) 2020 Craig Fowler
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+using System;
+namespace CSF
+{
+    /// <summary>
+    /// An object which can get a cache-key-provider implementation: <see cref="IGetsCacheKey"/>,
+    /// based upon an arbitrary collection of objects.
+    /// </summary>
+    public interface IGetsCacheKeyProvider
+    {
+        /// <summary>
+        /// Gets the cache key provider from the specified objects.
+        /// </summary>
+        /// <returns>The cache key provider.</returns>
+        /// <param name="objects">A collection of objects.</param>
+        IGetsCacheKey GetCacheKeyProvider(params object[] objects);
+    }
+}
